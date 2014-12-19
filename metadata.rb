d5debdd3d27c312535f22f7df24daa855fe6e481
jtimberman <joshua@chef.io>
2014-12-19 15:58:07 -0700

    update maintainer to CHEF

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
-maintainer        "Opscode, Inc."
-maintainer_email  "cookbooks@opscode.com"
+name "ruby"
+maintainer        "Chef Software, Inc."
+maintainer_email  "cookbooks@chef.io"
 license           "Apache 2.0"
 description       "Installs Ruby and related packages"
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.9.2"
 
 recipe "ruby", "Installs Ruby and related packages"
